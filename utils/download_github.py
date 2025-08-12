6d8c966b1080588cb11092aeddd3ca693065cf15
csxmli2016 <csxmli@gmail.com>
2025-08-13 00:00:32 +0800

    Update download_github.py

--- utils/download_github.py
+++ utils/download_github.py
@@ -1,5 +1,6 @@
 from basicsr.utils.download_util import load_file_from_url
 
+os.makedirs('./checkpoints', exist_ok=True)
 
 load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/bsrgan_bg.pth', model_dir='./checkpoints')
 load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/net_prior_860000.pth', model_dir='./checkpoints')
