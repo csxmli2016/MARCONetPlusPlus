43f075f8ff30aaaf4a8a195a4e572df0e76cb3cc
csxmli2016 <csxmli@gmail.com>
2025-08-12 23:03:05 +0800

    Add files via upload

---
+++ utils/download_github.py
@@ -0,0 +1,8 @@
+from basicsr.utils.download_util import load_file_from_url
+
+
+load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/bsrgan_bg.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/net_prior_860000.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/net_sr_860000.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/net_w_encoder_860000.pth', model_dir='./checkpoints')
+load_file_from_url('https://github.com/csxmli2016/MARCONetPlusPlus/releases/download/v1/yolo11m_short_character.pt', model_dir='./checkpoints')
